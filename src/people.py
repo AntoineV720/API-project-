18dedf3cdadf0890830f084c6589691c2a9632d3
Antoine Vialonga <antoine.vialonga@outlook.fr>
2023-04-13 17:22:37 +0200

    Add remaining files

---
+++ src/people.py
@@ -0,0 +1,11 @@
+class People:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+
+
+people1 = People("Jean",32)
+people2 = People("Mariane",34)
+
+
+#Exemple de python orienté objet
---
+++ src/run_sql.py
@@ -0,0 +1,28 @@
+from sqlalchemy import create_engine
+from sqlalchemy import text
+
+db_string='postgresql://root:root@localhost:5432/store'
+
+engine = create_engine(db_string)
+connection = engine.connect()
+
+create_table_query = """
+    CREATE TABLE IF NOT EXISTS films(
+        title text,
+        director text,
+        year text
+    );
+    """
+insert_data_query="""
+INSERT INTO films (title,director,year) VALUES ('Docteur Strange','Scott Derrickson','2016')
+"""
+#Create 
+# connection.execute(text('CREATE TABLE IF NOT EXISTS films (title text, director text, year text'))
+#connection.execute(text("INSERT INTO films (title, director, year) VALUES ('Doctor Strange', 'Scott Derickson')"))
+
+connection.execute(text(insert_data_query))
+connection.execute(text(create_table_query))
+connection.commit()
+
+connection.close()
+
